a18f6177d20fbbcf9dd96a890bc153c5223778b0
Tamjid Makhlukat Borno <158497150+Tamjid-Borno@users.noreply.github.com>
2024-02-27 00:08:32 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask, render_template, request, send_file
 from pytube import YouTube
+from moviepy.editor import AudioFileClip
 import os
+import platform
 
 app = Flask(__name__)
+
+def get_downloads_directory():
+    # Get the operating system
+    operating_system = platform.system()
+    # Get the user's home directory
+    user_home = os.path.expanduser("~")
+
+    # Determine the Downloads directory based on the operating system
+    if operating_system == 'Windows':
+        downloads_dir = os.path.join(user_home, 'Downloads')
+    elif operating_system == 'Linux':
+        downloads_dir = os.path.join(user_home, 'Downloads')
+    elif operating_system == 'Darwin':  # macOS
+        downloads_dir = os.path.join(user_home, 'Downloads')
+    else:
+        # Default to the user's home directory if the operating system is not recognized
+        downloads_dir = user_home
+    
+    return downloads_dir
 
 @app.route('/')
 def index():
@@ -13,19 +34,32 @@ def download():
     url = request.form['url']
     try:
         yt = YouTube(url)
-        video = yt.streams.filter(only_audio=True).first()
-        # Set the destination directory for the temporary file
-        temp_dir = '/temporary-cache'  # Specify your temporary directory
-        # Download the audio stream to the temporary directory
-        audio_file = video.download(output_path=temp_dir)
-        # Rename the downloaded file to have an MP3 extension
-        base, _ = os.path.splitext(audio_file)
-        mp3_file = base + ".mp3"
-        os.rename(audio_file, mp3_file)
-        # Send the MP3 file as an attachment for download
-        return send_file(mp3_file, as_attachment=True)
+        
+        # Get the audio stream
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        
+        # Determine the Downloads directory
+        downloads_dir = get_downloads_directory()
+
+        # Download the audio stream directly to the Downloads directory
+        audio_stream.download(output_path=downloads_dir, filename='audio.mp4')
+
+        # Convert the downloaded audio to MP3 using AudioFileClip
+        mp3_file = os.path.join(downloads_dir, 'audio.mp3')
+        mp4_file = os.path.join(downloads_dir, 'audio.mp4')
+
+        # Check if the MP4 file exists
+        if os.path.exists(mp4_file):
+            audio_clip = AudioFileClip(mp4_file)
+            audio_clip.write_audiofile(mp3_file)
+            audio_clip.close()
+            
+            # Send the MP3 file as an attachment for download
+            return send_file(mp3_file, as_attachment=True)
+        else:
+            return "Error: MP4 file not found"
     except Exception as e:
-        return str(e)
+        return "Error: " + str(e)
 
 if __name__ == '__main__':
     app.run(debug=True)
